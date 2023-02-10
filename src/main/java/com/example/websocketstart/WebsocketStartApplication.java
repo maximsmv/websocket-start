f98349313d015c788eb46515e7b1131981c71a74
maximsmv <maximsmv27@gmail.com>
2023-02-10 13:41:46 +0500

    Initial commit

---
+++ src/main/java/com/example/websocketstart/WebsocketStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websocketstart/WebsocketStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
